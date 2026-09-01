c8e5eb18b6bb21adde26fa4afcc688b411354422
Daniel <dmtz3471@gmail.com>
2026-09-01 13:47:28 -0700

    initial commit

---
+++ app/src/main/java/com/example/cst438_team1_project1/LoginScene.kt
@@ -0,0 +1,14 @@
+package com.example.cst438_team1_project1
+
+class LoginScene {
+    //created a login scene
+
+
+    //username logic
+
+
+    //password logic
+
+
+    //login button logic; pass through test API info - saved/ current cryptos
+}
