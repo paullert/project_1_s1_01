907d7ef87dae2c70de81aa678bc780b7c1a59a86
Lesly Villanueva <l3sly2022@gmail.com>
2026-09-01 20:44:26 -0700

    Main Activity.kt - added composable for sign up screen: shows username, password and confirmation password fields - basic layout of app

--- app/src/main/java/com/example/cst438_team1_project1/MainActivity.kt
+++ app/src/main/java/com/example/cst438_team1_project1/MainActivity.kt
@@ -3,6 +3,19 @@ package com.example.cst438_team1_project1
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -10,11 +23,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent{
+            SignUpScreen()
+        }
+    }
+}
+
+@Composable
+fun SignUpScreen() {
+    Column(modifier = Modifier.fillMaxSize().padding(top = 80.dp)){
+
+        Row(){
+            Text("Crypto-Tracker", fontSize = 40.sp, fontWeight = FontWeight.Bold)
+        }
+
+        Row(){
+            Text("Choose a Username:")
+            TextField( value ="enter new username",
+                onValueChange = {},
+                )
+        }
+
+        Row(){
+            Text("Choose a password:")
+            TextField( value ="enter password",
+                onValueChange = {},
+            )
+        }
+
+        Row(){
+            Text("Confirm password:")
+            TextField( value ="re-enter password",
+                onValueChange = {},
+            )
         }
     }
 }
