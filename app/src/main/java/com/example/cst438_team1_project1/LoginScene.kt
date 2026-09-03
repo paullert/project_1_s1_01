3908d4a628bea72d9f0a6444de8b6fdce6dc5ae2
Daniel <dmtz3471@gmail.com>
2026-09-02 22:17:01 -0700

    updated login page layout and LoginScene.kt

--- app/src/main/java/com/example/cst438_team1_project1/LoginScene.kt
+++ app/src/main/java/com/example/cst438_team1_project1/LoginScene.kt
@@ -1,15 +1,22 @@
 package com.example.cst438_team1_project1
 
-class LoginScene {
-    //create a login scene
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+class LoginScene : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_scene)
 
-    //username logic
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.loginButton)
 
-
-    //password logic
-
-
-    //login button logic; pass through test API info - saved/ current cryptos
-    //and sends to Home Page
+        loginButton.setOnClickListener {
+            loginButton.text = "Login button clicked!"
+        }
+    }
 }
