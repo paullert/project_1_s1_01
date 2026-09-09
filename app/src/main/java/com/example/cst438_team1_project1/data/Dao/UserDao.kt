ad2871a693da5e6fdaf6975977b71312d3f1f2d9
Lesly Villanueva <l3sly2022@gmail.com>
2026-09-09 07:45:49 -0700

    username now changes

--- app/src/main/java/com/example/cst438_team1_project1/data/Dao/UserDao.kt
+++ app/src/main/java/com/example/cst438_team1_project1/data/Dao/UserDao.kt
@@ -15,7 +15,7 @@ import com.example.cst438_team1_project1.data.entity.User
 @Dao
 interface UserDao {
     @Insert
-    suspend fun insertUser(user: User)
+    suspend fun insertUser(user: User): Long
     //^^ means the same as INSERT INTO USER (username, password) VALUES('user1','abc123')
 
     @Query("SELECT * FROM user WHERE username = :username LIMIT 1")
