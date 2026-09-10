9c252a5678899920a8982ff14be813b369ae9a9d
Paul Conner <paulconner.ca@gmail.com>
2026-09-10 13:16:38 -0700

    Added checkstyle, pmd, errorprone, and spotbugs

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,19 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.ksp) apply false
     id("androidx.room3") version "3.0.2" apply false
+    id("com.github.spotbugs") version "6.5.11"
+    checkstyle
+    pmd
+    id("net.ltgt.errorprone") version "5.1.1"
+}
+
+pmd {
+    toolVersion = "7.19.0"
+//    ruleSetFiles = files("custom-pmd-ruleset.xml")
+//    ruleSets = intArrayOf()
+    isIgnoreFailures = false
+}
+
+checkstyle {
+    toolVersion = "10.12.4"
 }
