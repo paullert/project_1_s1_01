78c9def0afbcb94857c961b0fb6151899b2df547
Lesly Villanueva <l3sly2022@gmail.com>
2026-09-16 16:50:14 -0700

    created tests for findByUsername, update Password, and deleteUser

--- app/src/test/java/com/example/cst438_team1_project1/UserDaoTest.kt
+++ app/src/test/java/com/example/cst438_team1_project1/UserDaoTest.kt
@@ -8,6 +8,8 @@ import com.example.cst438_team1_project1.data.AppDatabase
 import com.example.cst438_team1_project1.data.Dao.UserDao
 import com.example.cst438_team1_project1.data.entity.User
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNull
+import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
@@ -32,10 +34,20 @@ class UserDaoTest {
     @Test
     fun insertUserTest() = runTest {
         val user = User(username = "bloop1020", password = "password1")
-        val insertedId = userDao.insertUser(user).toInt()
-        val retrievedUser = userDao.findByUsername("bloop1020")
-        assert(retrievedUser != null)
-        assertEquals(insertedId, retrievedUser?.userId)
+        userDao.insertUser(user)
+
+        val savedUser = userDao.findByUsername("bloop1020")
+        assertEquals("bloop1020", savedUser?.username)
+        assertEquals("password1", savedUser?.password)
+    }
+
+    @Test
+    fun findByUsernameTest() = runTest {
+        val user = User(username = "bloop1020", password = "password1")
+        userDao.insertUser(user)
+        val foundUser = userDao.findByUsername("bloop1020")
+        assertEquals("bloop1020", foundUser?.username)
+        assertEquals("password1", foundUser?.password)
     }
 
     @Test
@@ -47,5 +59,28 @@ class UserDaoTest {
         val updatedUser = userDao.findByUsername("newUSERNAME")
         assert(updatedUser != null)
         assertEquals("newUSERNAME", updatedUser?.username)
+    }
+
+    @Test
+    fun updatePasswordTest() = runTest {
+        val user = User(username = "bloop1020", password = "password1")
+        val insertedId = userDao.insertUser(user).toInt()
+
+        userDao.updatePassword(insertedId, "newPASSWORD")
+        val updatedUser = userDao.findByUsername("bloop1020")
+        assert(updatedUser != null)
+        assertEquals("newPASSWORD", updatedUser?.password)
+    }
+
+    @Test
+    fun deleteUserTest() = runTest {
+        val user = User(username = "bloop1020", password = "password1")
+       val insertedId = userDao.insertUser(user).toInt()
+
+        val savedUser = user.copy(userId = insertedId)
+        userDao.deleteUser(savedUser)
+
+        val deletedUser = userDao.findByUsername("bloop1020")
+        assertNull(deletedUser)
     }
 }
