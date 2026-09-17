18de7c955095ef8e5b356b10c48f0f03b7c4b132
Paul <199071975+BoneDude@users.noreply.github.com>
2026-09-16 21:47:35 -0700

    Fixed API Key Reference from BuildConfig

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,7 @@
+import com.android.build.api.variant.BuildConfigField
+import org.gradle.kotlin.dsl.android
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -38,6 +42,8 @@ android {
     }
     buildFeatures {
         compose = true
+        // Allows local variables to be used in program
+        buildConfig = true
     }
 }
 
@@ -68,6 +74,9 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("androidx.datastore:datastore-preferences:1.2.1")
 
+    //Allows the use of internet images in composable
+    implementation("io.coil-kt.coil3:coil-compose:3.4.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.4.0")
 
     testImplementation(libs.junit)
 
@@ -86,4 +95,28 @@ dependencies {
 
 room3 {
     schemaDirectory("$projectDir/schemas")
+}
+
+//Below Code is used to populate app with API Key
+// Must provide the api key in the secret.properties file
+val secrets = Properties()
+val secretsFile = rootProject.file("secrets.properties")
+
+if (secretsFile.exists()) {
+    secretsFile.inputStream().use { secrets.load(it) }
+}
+
+val apiKey = secrets.getProperty("COINGECKO_API_KEY")
+    ?: System.getenv("COINGECKO_API_KEY")
+    ?: error("COINGECKO_API_KEY is missing")
+
+androidComponents.onVariants { variant ->
+    variant.buildConfigFields?.put(
+        "COINGECKO_API_KEY",
+        BuildConfigField(
+            type = "String",
+            value = "\"$apiKey\"",
+            comment = "API key"
+        )
+    )
 }
--- app/src/main/java/com/example/cst438_team1_project1/data/api/CoinGeckoAPI.kt
+++ app/src/main/java/com/example/cst438_team1_project1/data/api/CoinGeckoAPI.kt
@@ -1,45 +1,44 @@
 package com.example.cst438_team1_project1.data.api
 
+import com.example.cst438_team1_project1.data.api.api_responses.SearchCoinsResponse
 import retrofit2.http.Query
-import org.json.JSONObject
 import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.Header
 
-const val API_KEY = ""
 interface CoinGeckoAPI {
     // Retrieves Coins Via Generalized Search
     // API Description: "To search for coins, categories and markets listed on CoinGecko"
-    @Headers("x-cg-demo-api-key: $API_KEY")
     @GET("search")
-    suspend fun searchBy(
-        @Query("query") query: String
-    ): JSONObject
+    suspend fun searchCoins(
+        @Query("query") query: String,
+        @Header("x-cg-demo-api-key") apiKey: String
+    ): SearchCoinsResponse
 
     // Retrieves Coin prices via comma separated names
-    // Can be 1 or many
-    // EX: "Etherium, Bitcoin, Solana, <...>"
+    // Can be 1 or many, no spaces
+    // EX: "Etherium,Bitcoin,Solana,<...>"
     // EX: "Etherium"
     @GET("simple/price")
     suspend fun getCoinPricesByName(
         @Query("names") name: String
-    ): JSONObject
+    ): SearchCoinsResponse
 
     // Retrieves Coin prices via internal ids (kebabcase name)
-    // Can be 1 or many
-    // EX: "etherium, world-liberty-financial, <...>"
+    // Can be 1 or many, no spaces
+    // EX: "etherium,world-liberty-financial,<...>"
     // EX: "etherium"
     @GET("simple/price")
     suspend fun getCoinPricesByID(
         @Query("ids") ids: String
-    ): JSONObject
+    ): SearchCoinsResponse
 
     // Retrieves Coin prices via Ticker (3-4 letter combo)
-    // Can be 1 or many
-    // EX: "btc, sol, <...>"
+    // Can be 1 or many, no spaces
+    // EX: "btc,sol,<...>"
     // EX: "btc"
     @GET("simple/price")
     suspend fun getCoinPricesBySymbol(
         @Query("symbols") symbols: String
-    ): JSONObject
+    ): SearchCoinsResponse
 
 }
